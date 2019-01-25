1d84eacd07a7468c6050d9676739dc2f674a2d0a
Calvin445 <cai.ca@husky.neu.edu>
2019-01-25 16:42:45 -0500

    update tests

--- test/test.js
+++ test/test.js
@@ -1,93 +1,104 @@
 var log4js = require('log4js');
-var assert = require("assert");
+var assert = require('assert');
 
-describe('test filter', function(){
+describe('test filter', function() {
 
-	it ('should limit to 1 log', function(){
-		var logEvents = [];
-		log4js.clearAppenders();
-		var appender = require('../lib/filter')
-	        .appender({TRACE:1},
-	          1000,
-	          function(evt) { logEvents.push(evt); }
-	        );
-	      log4js.addAppender(appender, ["app"]);	
+	let recording = require('../node_modules/log4js/lib/appenders/recording.js');
+	const setUpTest = function(config) {
+		// set up the testing output.
+		log4js.configure({
+			appenders: {
+				test: {
+					type: 'recording'
+				},
+				rateFilter: {
+					type: 'lib/filter',
+					limits: config.limits,
+					interval: config.interval,
+					appender: config.appender
+				}
+			},
+			categories: {
+				default: {
+					appenders: ['rateFilter'],
+					level: 'ALL'
+				}
+			}
+		});
+	};
 
-	  	var logger = log4js.getLogger("app");    
-	  	logger.info('hey');
-	  	logger.info('hey');
-	  	logger.info('hey');
+	afterEach(function() {
+		recording.erase();
+	});
 
-	  	assert.equal(logEvents.length,1, 'Did not limit calls');
+
+	it ('should limit to 1 log', function() {
+		setUpTest({
+			limits: { TRACE: 1 },
+			interval: 1000,
+			appender: 'test'
+		})
+
+		var logger = log4js.getLogger("app");
+		logger.info('hey');
+		logger.info('hey');
+		logger.info('hey');
+
+		assert.equal(recording.replay().length,1, 'Did not limit calls');
 	});
 
 	it ('should limit to 1 error log', function(){
-		var logEvents = [];
-		log4js.clearAppenders();
-		var appender = require('../lib/filter')
-	        .appender({
-	        	TRACE:1,
-	        	ERROR:1
-	        },
-	          500,
-	          function(evt) { logEvents.push(evt); }
-	        );
-	      log4js.addAppender(appender, ["app"]);	
+		setUpTest({
+			limits: { TRACE: 1, ERROR: 1 },
+			interval: 500,
+			appender: 'test'
+		})
 
-	  	var logger = log4js.getLogger("app");    
-	  	logger.error('hey');
-	  	logger.info('hey');
-	  	logger.trace('hey');
-	  	
-	  	assert.equal(logEvents.length,1, 'Did not limit calls');
-	  	assert.equal(logEvents[0].level.level, log4js.levels.ERROR.level, 'Not correct error');
+		var logger = log4js.getLogger("app");
+		logger.error('hey');
+		logger.info('hey');
+		logger.trace('hey');
+
+		assert.equal(recording.replay().length,1, 'Did not limit calls');
+		assert.equal(recording.replay()[0].level, log4js.levels.ERROR, 'Not correct error');
 	});
 
 	it ('should limit to 2 logs', function(){
-		var logEvents = [];
-		log4js.clearAppenders();
-		var appender = require('../lib/filter')
-	        .appender({
-	        	TRACE:1,
-	        	ERROR:1
-	        },
-	          500,
-	          function(evt) { logEvents.push(evt); }
-	        );
-	      log4js.addAppender(appender, ["app"]);	
+		setUpTest({
+			limits: { TRACE: 1, ERROR: 1 },
+			interval: 500,
+			appender: 'test'
+		})
 
-	  	var logger = log4js.getLogger("app");    
-	  	logger.info('hey');
-	  	logger.error('hey');
-	  	logger.trace('hey');
-	  	
-	  	assert.equal(logEvents.length,2, 'Did not limit calls');
-	   assert.equal(logEvents[0].level.level, log4js.levels.INFO.level, 'Not correct error');
-	   assert.equal(logEvents[1].level.level, log4js.levels.ERROR.level, 'Not correct error');	
+		var logger = log4js.getLogger("app");
+		logger.info('hey');
+		logger.error('hey');
+		logger.trace('hey');
+
+		assert.equal(recording.replay().length,2, 'Did not limit calls');
+		assert.equal(recording.replay()[0].level, log4js.levels.INFO, 'Not correct error');
+		assert.equal(recording.replay()[1].level, log4js.levels.ERROR, 'Not correct error');
 	});
 
 	it ('should limit to 2 logs, with time limit', function(done){
-		var logEvents = [];
-		log4js.clearAppenders();
-		var appender = require('../lib/filter')
-	        .appender({TRACE:1},
-	          500,
-	          function(evt) { logEvents.push(evt); }
-	        );
-	      log4js.addAppender(appender, ["app"]);	
+		setUpTest({
+			limits: { TRACE: 1 },
+			interval: 500,
+			appender: 'test'
+		})
 
-	  	var logger = log4js.getLogger("app");    
-	  	logger.info('hey');
-	  	logger.info('hey');
-	  	logger.info('hey');
+		var logger = log4js.getLogger("app");
+		logger.info('hey');
+		logger.info('hey');
+		logger.info('hey');
 
-		assert.equal(logEvents.length,1, 'Did not limit calls');
-	  	setTimeout(function(){
+		assert.equal(recording.replay().length,1, 'Did not limit calls');
+		setTimeout(function(){
 			logger.info('hey');
-		  	logger.info('hey');
-		  	logger.info('hey');	  		
-		  	assert.equal(logEvents.length,2, 'Did not limit calls');
-		  	done();
-	  	}, 800);
+			logger.info('hey');
+			logger.info('hey');
+			assert.equal(recording.replay().length,2, 'Did not limit calls');
+			done();
+		}, 800);
 	});
 });
