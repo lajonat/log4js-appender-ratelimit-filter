349427a3aa8c918f263f9bec20f2b1375c4f7b53
Calvin445 <cai.ca@husky.neu.edu>
2019-01-28 13:32:14 -0500

    add tests for different configurations

--- test/test.js
+++ test/test.js
@@ -1,43 +1,45 @@
-var log4js = require('log4js');
-var assert = require('assert');
+const log4js = require('log4js');
+const assert = require('assert');
 
-describe('test filter', function() {
-
-	let recording = require('../node_modules/log4js/lib/appenders/recording.js');
-	const setUpTest = function(config) {
-		// set up the testing output.
-		log4js.configure({
-			appenders: {
-				test: {
-					type: 'recording'
-				},
-				rateFilter: {
-					type: 'lib/filter',
-					limits: config.limits,
-					interval: config.interval,
-					appender: config.appender
-				}
+const setUpTest = function(config) {
+	// set up the testing output.
+	log4js.configure({
+		appenders: {
+			myOwnAppender: {
+				type: "recording"
 			},
-			categories: {
-				default: {
-					appenders: ['rateFilter'],
-					level: 'ALL'
-				}
+			rateFilter: {
+				type: 'lib/filter',
+				limits: config.limits,
+				interval: config.interval,
+				appender: config.appender
+			},
+			definedAfter: {
+				type: "console"
 			}
-		});
-	};
+		},
+		categories: {
+			default: {
+				appenders: ['rateFilter'],
+				level: 'ALL'
+			}
+		}
+	});
+};
+
+const filterTests = function(appender) {
+	let recording = require('../node_modules/log4js/lib/appenders/recording.js');
 
 	afterEach(function() {
 		recording.erase();
 	});
 
-
 	it ('should limit to 1 log', function() {
 		setUpTest({
 			limits: { TRACE: 1 },
 			interval: 1000,
-			appender: 'test'
-		})
+			appender: appender
+		});
 
 		var logger = log4js.getLogger("app");
 		logger.info('hey');
@@ -47,12 +49,12 @@ describe('test filter', function() {
 		assert.equal(recording.replay().length,1, 'Did not limit calls');
 	});
 
-	it ('should limit to 1 error log', function(){
+	it ('should limit to 1 error log', function() {
 		setUpTest({
 			limits: { TRACE: 1, ERROR: 1 },
 			interval: 500,
-			appender: 'test'
-		})
+			appender: appender
+		});
 
 		var logger = log4js.getLogger("app");
 		logger.error('hey');
@@ -63,12 +65,12 @@ describe('test filter', function() {
 		assert.equal(recording.replay()[0].level, log4js.levels.ERROR, 'Not correct error');
 	});
 
-	it ('should limit to 2 logs', function(){
+	it ('should limit to 2 logs', function() {
 		setUpTest({
 			limits: { TRACE: 1, ERROR: 1 },
 			interval: 500,
-			appender: 'test'
-		})
+			appender: appender
+		});
 
 		var logger = log4js.getLogger("app");
 		logger.info('hey');
@@ -80,12 +82,12 @@ describe('test filter', function() {
 		assert.equal(recording.replay()[1].level, log4js.levels.ERROR, 'Not correct error');
 	});
 
-	it ('should limit to 2 logs, with time limit', function(done){
+	it ('should limit to 2 logs, with time limit', function(done) {
 		setUpTest({
 			limits: { TRACE: 1 },
 			interval: 500,
-			appender: 'test'
-		})
+			appender: appender
+		});
 
 		var logger = log4js.getLogger("app");
 		logger.info('hey');
@@ -100,5 +102,30 @@ describe('test filter', function() {
 			assert.equal(recording.replay().length,2, 'Did not limit calls');
 			done();
 		}, 800);
+	});
+};
+
+describe('test functionality from different configurations', function() {
+
+	describe('test using appenders defined in the configuration', () => filterTests('myOwnAppender'));
+
+	describe('test using core appenders', () => filterTests('recording'));
+
+	describe('test trying undefined appenders and breaking', function() {
+		it('should break with an undefined appender', function () {
+			try {
+				filterTests('undefinedAppender');
+			} catch (e) {
+				assert.equal(e.message, 'Could not find appender to filter. Configure the filter with a valid appender.')
+			}
+		});
+
+		it('should break with appenders defined after the filter', function() {
+			try {
+				filterTests('definedAfter');
+			} catch (e) {
+				assert.equal(e.message, 'Could not find appender to filter. Configure the filter with a valid appender.')
+			}
+		});
 	});
 });
